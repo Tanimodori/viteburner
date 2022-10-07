f542ba2d6d7e7bcf8fe743677802c4b8a167fa6f
Tanimodori <unknowner2014@gmail.com>
2022-10-07 15:27:13 +0800

    fix: fix resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import fg from 'fast-glob';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { resolve } from 'node:path';
 
 const entries = fg.sync('src/**/*.{js,ts}');
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
