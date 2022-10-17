d268424bde0bc270931fdc98b7b95e637f5076f6
Tanimodori <unknowner2014@gmail.com>
2022-10-17 15:58:56 +0800

    feat: watcher functional

--- build/watcher.ts
+++ build/watcher.ts
@@ -1,3 +1,27 @@
-console.log('Hello, world!');
+import { resolve } from 'node:path';
+import { createServer } from 'vite';
+import { ViteNodeServer } from 'vite-node/server';
+import { slash, normalizeRequestId } from 'vite-node/utils';
 
-export {};
+export function path2Id(file: string, base: string) {
+  const id = `/@fs/${slash(resolve(file))}`;
+  return normalizeRequestId(id, base);
+}
+
+export async function watch() {
+  const server = await createServer({
+    mode: 'development',
+    optimizeDeps: {
+      disabled: true,
+    },
+  });
+
+  await server.pluginContainer.buildStart({});
+
+  const node = new ViteNodeServer(server);
+
+  const result = await node.fetchModule(path2Id('src/template.ts', server.config.base));
+  console.log(result);
+}
+
+watch();
