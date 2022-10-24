2ba003d82320bac4d817eb19b25a30a22db97279
Tanimodori <unknowner2014@gmail.com>
2022-10-24 15:09:08 +0800

    feat: import fix

--- src/ws/import.ts
+++ src/ws/import.ts
@@ -1,5 +1,7 @@
 import { parse as acornParse } from 'acorn';
-import { WatchManager } from '..';
+import MagicString from 'magic-string';
+import { dirname, relative } from 'path';
+import { logger, WatchManager } from '..';
 
 export interface FixImportPathOptions {
   filename: string;
@@ -16,6 +18,24 @@ function parse(code: string) {
   });
 }
 
+function getFilename(options: FixImportPathOptions, imports?: string) {
+  const realImports = imports ?? options.filename;
+  const importPath = options.manager.getUploadFilenamesByServer(realImports, options.server);
+  if (!importPath) {
+    const warnInfo = imports
+      ? `Path "${imports}" imported by "${options.filename}" not found on server "${options.server}"`
+      : `File "${imports}" not found on server "${options.server}"`;
+    logger.warn(`import`, warnInfo);
+    logger.warn(`import`, `This may be a problem when the script is running on the server.`);
+    return {
+      filename: realImports,
+      server: options.server,
+    };
+  } else {
+    return importPath;
+  }
+}
+
 /**
  * Converts absolute path to relative path by the watch options.
  * Vite resolves import path to absolute path to source file.
@@ -25,13 +45,23 @@ function parse(code: string) {
  * We also need to returns a sourcemap.
  */
 export function fixImportPath(options: FixImportPathOptions) {
+  const magicString = new MagicString(options.content);
   const estree = parse(options.content);
+  const rootPath = getFilename(options);
 
   for (const statement of estree.body) {
     if (statement.type === 'ImportDeclaration') {
       const source = statement.source;
       const value = source.value as string;
       console.log(source);
+      // get the import path on the server
+      const importPath = getFilename(options, value);
+      // get the relative path
+      let relativePath = relative(dirname(rootPath.filename), importPath.filename);
+      if (!relativePath.startsWith('..')) {
+        relativePath = './' + relativePath;
+      }
+      console.log(relativePath);
     }
   }
 
