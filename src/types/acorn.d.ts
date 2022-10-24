a3d309b7978125f2698609024ae50d3c733ae2af
Tanimodori <unknowner2014@gmail.com>
2022-10-24 22:19:55 +0800

    chore: update acorn

--- src/types/acorn.d.ts
+++ src/types/acorn.d.ts
@@ -2,11 +2,6 @@ import * as ESTree from 'estree';
 
 declare module 'acorn' {
   export function parse(s: string, o: Options): ESTree.Program;
-
-  // fix type of Comment property 'type'
-  export type AcornComment = Omit<Comment, 'type'> & {
-    type: 'Line' | 'Block';
-  };
 }
 
 declare module 'estree' {
