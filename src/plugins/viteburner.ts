669154568f93761e8a4a5fae1d26072b77126856
Tanimodori <unknowner2014@gmail.com>
2022-10-28 11:03:03 +0800

    feat: viteburner plugin

--- src/plugins/viteburner.ts
+++ src/plugins/viteburner.ts
@@ -1,8 +1,14 @@
-import { Plugin, UserConfig, ViteDevServer } from 'vite';
+import { Plugin, UserConfig } from 'vite';
 import defu from 'defu';
 import { loadConfig } from 'unconfig';
-import { ViteBurnerConfig, ViteBurnerInlineConfig } from '@/types';
+import { HmrData, ViteBurnerConfig, ViteBurnerInlineConfig, ViteBurnerServer } from '@/types';
 import { resolveConfig } from '@/config/resolve';
+import { logger } from '@/console';
+import { WsManager, WsAdapter } from '@/ws';
+import { resolve } from 'pathe';
+import { slash, normalizeRequestId } from 'vite-node/utils';
+import { handleKeyInput, hmrPluginName } from '..';
+import { WatchManager } from './watch';
 
 export const virtualModuleId = 'virtual:viteburner-entry';
 
@@ -27,7 +33,7 @@ export function getDefaultConfig() {
 
 export function viteburnerPlugin(inlineConfig: ViteBurnerInlineConfig): Plugin {
   const resolvedVirtualModuleId = '\0' + virtualModuleId;
-  let server: ViteDevServer;
+  let server: ViteBurnerServer;
 
   return {
     name: 'viteburner',
@@ -50,11 +56,51 @@ export function viteburnerPlugin(inlineConfig: ViteBurnerInlineConfig): Plugin {
     },
     // save server instance
     configureServer(_server) {
-      server = _server;
+      server = {
+        ..._server,
+        pathToId(file: string) {
+          const id = `/@fs/${slash(resolve(server.config.root, file))}`;
+          return normalizeRequestId(id, server.config.base);
+        },
+        async invalidateFile(file: string) {
+          const id = server.pathToId(file);
+          const module = await server.moduleGraph.getModuleByUrl(id);
+          if (module) {
+            server.moduleGraph.invalidateModule(module);
+          }
+        },
+        async fetchModule(file: string) {
+          const id = server.pathToId(file);
+          return server.transformRequest(id);
+        },
+        onHmrMessage(handler: (data: HmrData, server: ViteBurnerServer) => void) {
+          server.watchManager.emitter.on(hmrPluginName, (data: HmrData) => handler(data, server));
+        },
+        async buildStart() {
+          return server.pluginContainer.buildStart({});
+        },
+      } as ViteBurnerServer;
     },
     // main entry
     buildStart() {
-      // TODO
+      // create ws server
+      logger.info('ws', 'creating ws server...');
+      const wsManager = new WsManager(server.config.viteburner);
+      const wsAdapter = new WsAdapter(wsManager, server);
+
+      // create watch
+      server.onHmrMessage(wsAdapter.handleHmrMessage);
+      const { root, viteburner } = server.config;
+      const { watch, ignoreInitial } = viteburner;
+      server.watchManager = new WatchManager(watch, {
+        cwd: root,
+        persistent: true,
+        ignoreInitial,
+      });
+      server.watchManager.init();
+
+      // create key handler
+      handleKeyInput(wsAdapter);
     },
     // virtual entry
     resolveId(id: string) {
