92cdefbf8932a2ae8998555fe930d4a002529e33
Tanimodori <unknowner2014@gmail.com>
2022-11-25 21:28:34 +0800

    test: add external import test

--- playground/src/importExternal/main.ts
+++ playground/src/importExternal/main.ts
@@ -3,5 +3,5 @@ import lodash from 'https://unpkg.com/lodash@4.17.21/lodash.min.js';
 import { NS } from '@ns';
 
 export async function main(ns: NS) {
-  console.log(lodash);
+  ns.tprint(lodash);
 }
